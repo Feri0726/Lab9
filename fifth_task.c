a336a0cdda11882c04bb44c3aadcb83acddf0015
Feri0726 <feherferi2007@gmail.com>
2025-11-05 17:02:31 +0000

    fifth done partly

--- fifth_task.c
+++ fifth_task.c
@@ -10,6 +10,29 @@ containing hello, world!: the second string has been inserted between letters "o
 
 #include <stdio.h> //don't forget to include the required libraries
 
+char *insert(char* original, int first, char* news){
+  int len1 = strlen(original);
+  int len2 =  strlen(news);
+  char* reworked = (char*)malloc(sizeof(char)*(len1+len2+1));
+  int j = 0;
+  for (int i = 0; i < (len1+len2+first); i++){
+    if (i < first){
+      reworked[i] = original[i];
+    }
+    if ((i >= first) && (i < first + len2)){
+      reworked[i] = news[j];
+      j++;
+    }
+    if ((i >= first +len2) && (i < len1 + len2)){
+      reworked[i] = original[i-len2];
+    }
+    reworked[len1+len2] = '\0';
+    return reworked;
+
+}
+
+
+
 int main()
 {
 
